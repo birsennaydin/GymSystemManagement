752c95bbcb70b309e404f4508766ea7a8ef4b4e7
birsenaydin <bsnaydin@gmail.com>
2025-01-06 16:27:02 +0000

    chore: change github configuration

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,8 +1,5 @@
 # tests/test_user.py
 import unittest
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'models')))
 from models.user import User
 from models.enums import UserRole
 
